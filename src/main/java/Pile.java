280b76c168ce4f7b930f58583f534451d4923e77
Andrew <andrew.p_pro@outlook.com>
2022-02-27 12:13:50 +0100

    Envoi de projet

---
+++ src/main/java/Pile.java
@@ -0,0 +1,86 @@
+/**
+ * Classe permettant de g�rer une Pile de valeurs enti�res.
+ * L'acc�s est LIFO (Last In First Out).
+ * Exemple :
+		 Pile p;
+		 p = new Pile();
+		 p.empiler(10);
+		 p.empiler(20);
+		 System.out.println(p.sommet()); // 20
+		 p.depiler();
+		 System.out.println(p.sommet()); // 10
+		 System.out.println(p.estVide()); // false
+ */
+public class Pile {
+	private int[] elements; // tableau des �l�ments contenus dans la Pile
+	private int indiceSommet; // indice du dernier �l�ment empil�, -1 => pile est vide
+	
+	/**
+	 * Constructeur par d�faut.
+	 * Construit une Pile de taille 100 maxi.
+	 */
+	public Pile() {
+		this.elements = new int[100];
+		this.indiceSommet = -1;
+	}
+
+	/**
+	 * Constructeur par d�faut.
+	 * Construit une Pile de taille 100 maxi.
+	 * @param pfTaille IN la taille max de la pile
+	 */
+	public Pile(int pfTaille) {
+		this.elements = new int[pfTaille];
+		this.indiceSommet = -1;
+	}
+
+	/**
+	 * Indique si la Pile est pleine (true) ou pas.
+	 * @return true si la Pile est pleine
+	 */
+	public boolean estPleine() {
+		return (this.indiceSommet == this.elements.length-1);
+	}
+	
+	/**
+	 * Empile pfElement sur le haut de la Pile.
+	 * @param pfElement
+	 * @see Pile#estPleine()
+	 * Pr�condition : la pile n'est pas pleine
+	 */
+	public void empiler(int pfElement) {
+		this.indiceSommet = this.indiceSommet + 1;
+		this.elements[this.indiceSommet] = pfElement;
+	}
+
+	/**
+	 * Indique si la Pile est vide (true) ou pas.
+	 * @return true si la Pile est vide
+	 */
+	public boolean estVide() {
+		return (this.indiceSommet == -1);
+	}
+
+	/**
+	 * D�pile le sommet de la Pile.
+	 * @see Pile#estVide()
+	 * Pr�condition : la pile n'est pas vide.
+	 */
+	public void depiler()  {
+		this.indiceSommet = this.indiceSommet - 1;
+	}
+
+	/**
+	 * Permet d'obtenir la valeur contenu au sommet de la Pile.
+	 * 
+	 * @see Pile#estVide()
+	 * Pr�condition : la pile n'est pas vide.
+	 * @return la valeur au sommet de la Pile
+	 */
+	public int sommet() {
+		return (this.elements[this.indiceSommet]);
+	}
+	public static void main(String[] args){
+		System.out.println("Bonjour");
+	}
+}
